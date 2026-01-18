283534d38ee6c5571294e29aa7129b1fb42ce7df
Radyt-Thya <2401024a@student.tp.edu.sg>
2026-01-18 22:11:34 +0800

    Updated commenting issues, minor changes to FAQ and staff dashboard

--- TechReviewzWebsite/Domain/Comments.cs
+++ TechReviewzWebsite/Domain/Comments.cs
@@ -2,9 +2,8 @@
 {
     public class Comments : BaseDomainModel
     {
-       
-        public string Content { get; set; } = string.Empty;
 
+        public string Content { get; set; } = string.Empty;
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
         public string? UserId { get; set; }
